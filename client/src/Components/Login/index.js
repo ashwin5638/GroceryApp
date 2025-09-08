4db7f346a32f2871941dad6659586174bbe90974
Ashwin <138807279+ashwin5638@users.noreply.github.com>
2025-09-08 22:44:12 +0530

    Update index.js

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -42,7 +42,7 @@ const Login = () => {
 
     try {
       // Fixed: Changed URL from /Users/login to /login to match backend
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://groceryapp-backend-3mgw.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
